88168e2bf986f67681d834f0fe3d5bfb10df99d3
Grigorie Ifrim <48922918+geogeo20@users.noreply.github.com>
2019-09-26 06:00:12 +0300

    TileFirstSetUp

---
+++ BuildTile/Assets/Model/World.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World
+{
+    Tile[,] tiles;
+    int width;
+    int height;
+
+
+    public World(int width = 100, int height = 100)
+    {
+        this.width = width;
+        this.height = height;
+
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tiles[x, y] = new Tile(this, x, y);
+            }
+        }
+
+    }
+
+    public Tile GetTileAt(int x, int y)
+    {
+        if (x > width || x < 0 || y > height || y < 0)
+        {
+            Debug.LogError("Tile ( " + x + " " + y + " ) is out of bounds");
+        }
+
+
+        return tiles[x, y];
+    }
+
+
+
+}
